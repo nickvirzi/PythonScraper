0c752a26660ef43429c5a72801694af241f7f516
nickvirzi <71466589+nickvirzi@users.noreply.github.com>
2021-02-16 16:49:19 -0500

    Added First Changes

---
+++ sportsScraper.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+DRIVER_PATH = r'C:\Users\navir\Documents\ChromeDriver\chromedriver.exe'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://www.basketball-reference.com/boxscores')
+
+date = driver.find_element_by_xpath('//*[@id="content"]/div[1]/span').text
+
+dave = 0
